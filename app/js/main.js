9dbe402e1d1ba2cf824609fa6394eb5c0e4a5a5e
Stevey Z <sjzerneri@gmail.com>
2016-04-24 11:27:22 -0400

    adding welcome page

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,35 @@
-angular.module('SampleApp', ['ngRoute', 'ngAnimate'])
+(function () {
 
-.config([
+    'use strict';
+
+
+    angular.module('SampleApp', ['ngRoute', 'ngAnimate'])
+
+    .config([
         '$locationProvider',
         '$routeProvider',
         function ($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
-        // routes
-        $routeProvider
-            .when("/", {
-                templateUrl: "./partials/partial1.html",
-                controller: "MainController"
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+            $locationProvider.hashPrefix('!');
+            // routes
+            $routeProvider
+                .when("/", {
+                    templateUrl: "./partials/welcome.html",
+                    controller: "MainController"
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
         }
     ]);
 
-//Load controller
-angular.module('SampleApp')
+    //Load controller
+    angular.module('SampleApp')
 
-.controller('MainController', [
+    .controller('MainController', [
         '$scope',
         function ($scope) {
-        $scope.test = "Testing...";
+            $scope.test = "Welcome to the Countries and Capitals App. I hope you have a lot of fun learning about this information. If you have any questions, we don't have a support team, so feel free to figure it out yourself :)";
         }
     ]);
+
+}());
