1b0c4868ccddd48423144886f78c38230aee923f
Stevey Z <sjzerneri@gmail.com>
2016-04-26 21:06:08 -0400

    latest commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 //inside of Gulpfile.js
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-gulp.task('connect', function () {
-    connect.server();
+
+gulp.task('hello', function () {
+    console.log('Hello Zell');
 });
