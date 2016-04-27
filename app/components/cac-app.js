55e5dc544c6c9db88e1a25573861c0c9ac77110c
Stevey Z <sjzerneri@gmail.com>
2016-04-27 10:44:04 -0400

    adding countries page and configuring api call

--- app/components/cac-app.js
+++ app/components/cac-app.js
@@ -1,9 +1,38 @@
 angular.module('cacApp', ['ngRoute', 'ngAnimate'])
     .config(function ($routeProvider) {
         $routeProvider.when('/', {
-            controller: 'mainController'
-        })
+                templateUrl: 'welcome.html',
+                controller: 'mainController'
+            })
+            .when('/countries', {
+                templateUrl: 'countries.html',
+                controller: 'mainController'
+            })
     })
-    .controller('mainController', function ($rootScope, $scope) {
+    .controller('mainController', function ($rootScope, $scope, $location, $http, MyCache) {
         $scope.test = "Testing...";
+        $scope.countriesTest = "Testing Countries....";
+        $scope.browseCountries =
+
+            function (path) {
+                $location.path(path);
+            };
+
+        $http.get('http://api.geonames.org/countryInfoJSON?username=steveyz', {
+                cache: MyCache
+            })
+            .then(function (response) {
+
+                console.log(response.data);
+                console.log(response.data.geonames);
+
+
+            }, function (response) {
+                console.log('Geo Response Failed');
+            });
+
+
     })
+    .factory('MyCache', function ($cacheFactory) {
+        return $cacheFactory('myCache');
+    });
