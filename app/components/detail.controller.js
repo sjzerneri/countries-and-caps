8e9bea1dd7d1a97ba684174776260f39bfd171b0
Stevey Z <sjzerneri@gmail.com>
2016-05-06 22:35:37 -0400

    comsolidating factories

--- app/components/detail.controller.js
+++ app/components/detail.controller.js
@@ -1,33 +1,33 @@
-(function(){
-angular.module('cacApp')
-    .controller('detailController', function ($scope, $location, countrydetails, $routeParams, $q) {
+(function () {
+    angular.module('cacApp')
+        .controller('detailController', function ($scope, $location, $routeParams, $q, geonames) {
 
-        $scope.browseCountries =
+            $scope.browseCountries =
 
-            function (path) {
-                $location.path(path);
-            };
+                function (path) {
+                    $location.path(path);
+                };
 
-        var vm = this;
+            var vm = this;
 
-        vm.countryCode = $routeParams.countryCode;
+            vm.countryCode = $routeParams.countryCode;
 
-        vm.country = {};
+            vm.country = {};
 
-        $scope.code = $routeParams.countryCode;
+            $scope.code = $routeParams.countryCode;
 
-        countrydetails.countryDetails(vm.countryCode)
-            .then(function (response) {
-                vm.country = response;
-                return $q.all([
-                    countrydetails.search(vm.country.capital),
-                    countrydetails.neighbors(vm.country.geonameId)
+            geonames.countryDetails(vm.countryCode)
+                .then(function (response) {
+                    vm.country = response;
+                    return $q.all([
+                    geonames.search(vm.country.capital),
+                    geonames.neighbors(vm.country.geonameId)
                     ]);
-            }).then(function (response) {
-                vm.country.search = response[0];
-                vm.country.neighbors = response[1].geonames;
-                console.log(vm.country.neighbors);
-            })
+                }).then(function (response) {
+                    vm.country.search = response[0];
+                    vm.country.neighbors = response[1].geonames;
+                    console.log(vm.country.neighbors);
+                })
 
-    });
+        });
 }());
--- app/components/geonames.factory.js
+++ app/components/geonames.factory.js
@@ -1,21 +1,70 @@
-(function(){
-angular.module('cacApp')
-    .factory('geonames', function ($http) {
-        function countries() {
-            return $http.get('http://api.geonames.org/countryInfoJSON?username=steveyz', {
-                    cache: true
-                })
-                .then(function (response) {
+(function () {
+    angular.module('cacApp')
+        .factory('geonames', function ($http, $q) {
+            function countries() {
+                return $http.get('http://api.geonames.org/countryInfoJSON?username=steveyz', {
+                        cache: true
+                    })
+                    .then(function (response) {
 
-                    return response.data.geonames;
+                        return response.data.geonames;
 
-                }, function (response) {
-                    console.log('Geo Response Failed');
-                });
-        }
+                    }, function (response) {
+                        console.log('Geo Response Failed');
+                    });
+            }
 
-        return {
-            countries: countries
-        }
-    })
+            var baseUrl = 'http://api.geonames.org';
+            var username = 'steveyz';
+
+            function countryDetails(countryCode) {
+                var config = {
+                    params: {
+                        username: username,
+                        country: countryCode
+                    }
+                };
+
+                return $http.get(baseUrl + '/countryInfoJSON', config)
+                    .then(function (res) {
+                        return $q.when(res.data.geonames[0]);
+                    });
+            }
+
+            function search(query) {
+                var config = {
+                    params: {
+                        username: username,
+                        q: query
+                    }
+                };
+
+                return $http.get(baseUrl + '/searchJSON', config)
+                    .then(function (res) {
+                        return $q.when(res.data.geonames[0]);
+                    });
+            }
+
+            function neighbors(geonameId) {
+                var config = {
+                    params: {
+                        username: username,
+                        geonameId: geonameId
+
+                    }
+                };
+
+                return $http.get(baseUrl + '/neighboursJSON?', config)
+                    .then(function (res) {
+                        return $q.when(res.data);
+                    });
+            }
+
+            return {
+                countries: countries,
+                countryDetails: countryDetails,
+                search: search,
+                neighbors: neighbors
+            }
+        })
 }());
